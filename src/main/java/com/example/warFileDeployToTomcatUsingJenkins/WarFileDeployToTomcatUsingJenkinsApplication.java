efd70dc11e12b11062351f3935cadaf4f2fe1e95
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-05 09:15:21 +0200

    Initial commit

---
+++ src/main/java/com/example/warFileDeployToTomcatUsingJenkins/WarFileDeployToTomcatUsingJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.warFileDeployToTomcatUsingJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarFileDeployToTomcatUsingJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarFileDeployToTomcatUsingJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warFileDeployToTomcatUsingJenkins/WarFileDeployToTomcatUsingJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warFileDeployToTomcatUsingJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarFileDeployToTomcatUsingJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
